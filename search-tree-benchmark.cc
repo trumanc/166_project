39cf223019131a95be9ef94ed97f3c62188a98e3
Brandon Azad <bka314159@gmail.com>
2016-05-30 23:14:17 -0700

    Add COTree to new testing framework

--- search-tree-benchmark.cc
+++ search-tree-benchmark.cc
@@ -1,59 +1,147 @@
-#include <set>
-#include <iostream>
+#include <cstdlib>
+
+#include "btree_set.h"
+#include "vEB-tree.h"
+#include "cotree.h"
 
 using namespace std;
 
-#include <stdlib.h>
-#include "btree_set.h"
-#include "vEB-tree.h"
+struct IntParams : public cotree::cotree_params_tag {
+  typedef int value_type;
+  static int compare(int a, int b) {
+    return a - b;
+  }
+  static bool is_present(int a) {
+    return a != absent_value();
+  }
+  static int absent_value() {
+    return -1;
+  }
+};
 
-bool contains(set<int>& s, int n) {
-  return s.find(n) != s.end();
+bool calculate_unsigned(const char * str, size_t& value) {
+  const int stack_max = 4;
+  size_t stack[stack_max + 1];
+  int p = -1;
+  const char * s = str;
+  char * end;
+
+  // Implement little RPN calculator.
+  while (*s) {
+    switch (*s) {
+    case '^':
+      if (p < 0) {
+        return false;
+      }
+      stack[p] = 1 << stack[p];
+      s++;
+      break;
+    case '+':
+      if (--p < 0) {
+        return false;
+      }
+      stack[p] += stack[p + 1];
+      s++;
+      break;
+    case '-':
+      if (--p < 0) {
+        return false;
+      }
+      stack[p] -= stack[p + 1];
+      s++;
+      break;
+    case '*':
+      if (--p < 0) {
+        return false;
+      }
+      stack[p] *= stack[p + 1];
+      s++;
+      break;
+    case '/':
+      if (--p < 0) {
+        return false;
+      }
+      stack[p] /= stack[p + 1];
+      s++;
+      break;
+    default:
+      if (p == stack_max) {
+        return false;
+      }
+      stack[++p] = strtoull(s, &end, 0);
+      assert(s != end);
+      s = end;
+    }
+  }
+  if (p != 0) {
+    return false;
+  }
+  value = stack[0];
+  return true;
 }
 
-bool contains(btree::btree_set<int>& s, int n) {
-  return s.find(n) != s.end();
+enum TreeType {
+  VebTreeType,
+  COTreeType,
+  BTreeType
+};
+
+bool parse_tree_type(const char * str, TreeType& type) {
+  string s = string(str);
+  if (s == "VebTree") {
+    type = VebTreeType;
+  } else if (s == "COTree") {
+    type = COTreeType;
+  } else if (s == "BTree") {
+    type = BTreeType;
+  } else {
+    return false;
+  }
+  return true;
 }
+
+void usage(int argc, char* argv[]) {
+  cout << "Usage: " << argv[0] << " <tree-size> <num-queries> <tree-type>" << endl;
+  exit(1);
+}
+
+typedef cotree::cotree<IntParams> COTree;
+typedef btree::btree_set<int, std::less<int>, std::allocator<int>, 64> BTree;
 
 int main(int argc, char* argv[]) {
-  vector<int> v;
-  set<int> s;
-  bool VEB = (string(argv[1]) == "vebtree");
-  if (VEB) {
-    cout << "Using the vEB tree" << endl;
-    for (int i = 1; i <= 65536; i++) {
-      v.push_back(i *2);
-    }
+  size_t tree_size;
+  size_t num_queries;
+  TreeType type;
+  if (argc != 4 ||
+      !calculate_unsigned(argv[1], tree_size) ||
+      !calculate_unsigned(argv[2], num_queries) ||
+      !parse_tree_type(argv[3], type)) {
+    usage(argc, argv);
   }
-  v.push_back(200000);
-  v.push_back(200001);
-  VebTree t(v);
-  
-  btree::btree_set<int, std::less<int>, std::allocator<int>, 64> bts;
-  if (!VEB) {
-    cout << "Using the B-tree" << endl;
-    for (int i = 1; i <= 65536; i++) {
-      bts.insert(i *2);
-    }
-    bts.insert(200000);
-    bts.insert(200001);
+
+  vector<int> v(tree_size, 0);
+  for (int i = 0; i < tree_size; i++) {
+    v[i] = (i+1) * 2;
   }
-  int errors = 0;
+
+  VebTree vebtree(v);
+  BTree btree(v.begin(), v.end());
+  COTree cotree(v);
+
   int found = 0;
-  
-  if (string(argv[2]) == "query") {
-    cout << "Querying..." << endl;
-    int numTrials = 1 << 21;
-    for (int n = 0; n < numTrials; n++) {
-      int r = random() % 140000;
-      //if (contains(s, r) != (2000 <= r && r <= 20000 && r % 2 == 0)) {
-      if (VEB) {
-        if (t.contains(r)) found++;
-      } else {
-        if (bts.find(r) != bts.end()) found++;
-      }
+  for (int n = 0; n < num_queries; n++) {
+    int r = random() % (2 * tree_size);
+    switch (type) {
+      case VebTreeType:
+        if (vebtree.contains(r)) found++;
+        break;
+      case COTreeType:
+        if (cotree.contains(r)) found++;
+        break;
+      case BTreeType:
+        if (btree.find(r) != btree.end()) found++;
+        break;
     }
   }
-  cout << "found: " << found << endl;
-  return 0;
+  return found;
 }
